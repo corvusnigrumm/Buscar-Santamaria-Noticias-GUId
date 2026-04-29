9ba0aa787cbb20a4ef7326871cefb2c219c30816
corvusnigrumm <corvusnigrumm@users.noreply.github.com>
2026-04-29 00:09:15 -0500

    fix: correct logger import path in ai_tags

--- core/ai_tags.py
+++ core/ai_tags.py
@@ -3,7 +3,7 @@ import json
 import asyncio
 import hashlib
 import time
-from util.logger import log
+from core.logger import log
 from core.filters import DATA_APP_DIR
 
 CACHE_FILE = os.path.join(DATA_APP_DIR, "ai_tags_cache.json")
